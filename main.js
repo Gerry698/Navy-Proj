ade299d0027b3b8d198baf9c80f4572c376325c1
Jayden Ai <jayden.ai.2007@gmail.com>
2023-07-24 10:18:28 -0400

    framework

---
+++ main.js
@@ -0,0 +1,14 @@
+//The Compliment thingy
+let complimentbank = [""]
+let mrmikecomplimentbank = ["You're cruising through this!", "Wow! You did that so quickly! That was magic!", "Ben bought cookies, you all can have a piece.", "Yay""]
+
+
+
+
+
+//game
+
+
+
+
+//satisfying thing
