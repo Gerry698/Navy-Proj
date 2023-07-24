620a761a5fed349b477dff3ed0a08a5957873c8d
Jayden Ai <jayden.ai.2007@gmail.com>
2023-07-24 10:43:47 -0400

    more

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //The Compliment thingy
-let complimentbank = [""]
-let mrmikecomplimentbank = ["You're cruising through this!", "Wow! You did that so quickly! That was magic!", "Ben bought cookies, you all can have a piece.", "Yay""]
-
-
-
+let complimentbank = ["You're that “Nothing” when people ask me what I'm thinking about.", "You're gorgeous — and that's the least interesting thing about you, too.", "Is it getting warm in here, or is it just you, hot stuff?", ""];
+let mrmikecomplimentbank = ["You're cruising through this!", "Wow! You did that so quickly! That was magic!", "Ben bought cookies, you all can have a piece.", "Yay"];
+let rando = [Math.floor(Math.random() * (6))];
+addEventListener("")
+display()
 
 
 //game
